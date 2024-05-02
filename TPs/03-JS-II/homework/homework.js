017275181e6faa93612fa083c59cd44246640116
Tochigu-san <141269510+Tochigu-san@users.noreply.github.com>
2024-05-02 03:08:12 -0300

    Actualizados un par de ejercicios

--- TPs/03-JS-II/homework/homework.js
+++ TPs/03-JS-II/homework/homework.js
@@ -5,7 +5,7 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-  if (x <  y) {return x;
+  if (x >  y) {return x;
   } else {if (x < y) {return y;}
  else {if (x === y) {return x, y;}}
 }
@@ -28,6 +28,12 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  var estado;
+if (estado === 1) {
+  return 'Online';
+} else if (estado === 2) {
+  return 'Away';
+} else {return 'Offline';}
 }
 
 function saludo(idioma) {
